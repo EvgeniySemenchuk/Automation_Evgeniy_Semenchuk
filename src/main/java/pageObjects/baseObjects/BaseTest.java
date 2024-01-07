918d57d7aba4101eac7670ad7e890389fc4784c9
EvgeniySemenchuk <evgeniy.semenchuk.2004@gmail.com>
2024-01-07 22:43:02 +0300

    Add  lesson10-11 homework changes to GitHub

--- src/main/java/pageObjects/baseObjects/BaseTest.java
+++ src/main/java/pageObjects/baseObjects/BaseTest.java
@@ -4,6 +4,7 @@ import driver.DriverTypes;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
+import testngUtils.InvokedListener;
 import testngUtils.Listener;
 
 
@@ -12,7 +13,7 @@ import static driver.DriverCreation.*;
 import static driver.DriverTypes.*;
 import static driver.DriverTypes.CHROME;
 import static propertyUtils.PropertyReader.getProperties;
-@Listeners(Listener.class)
+@Listeners({Listener.class, InvokedListener.class})
 public class BaseTest {
 
     @BeforeTest
